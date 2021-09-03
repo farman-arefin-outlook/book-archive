fac3052dfad32427c3ff5f701e94ad280fa3e287
Tamim <farman.arefin1@outlook.com>
2021-09-03 22:24:37 +0600

    Final Commit

--- JS/api.js
+++ JS/api.js
@@ -1,51 +1,47 @@
-// //Search area https://covers.openlibrary.org/b/id/{cover_i}-M.jpg
+/* //Search area https://covers.openlibrary.org/b/id/{cover_i}-M.jpg
 
-// const searchField = document.getElementById('search-field');
-// const authorDetails = document.getElementById('author-details');
-// const bookNumber = document.getElementById('book-numbers');
-// const searchResult = document.getElementById('search-result');
+const searchField = document.getElementById('search-field');
+const authorDetails = document.getElementById('author-details');
+const bookNumber = document.getElementById('book-numbers');
+const searchResult = document.getElementById('search-result');
 
-// const loadImage = async (cover_i) => {
-//     const url = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     console.log(data);
+const loadImage = async (cover_i) => {
+    const url = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
 
-// }
+}
 
-// const searchBook = async () => {
-//     searchText = searchField.value;
-//     //console.log(searchText);
-//     const url = `https://openlibrary.org/search.json?q=${searchText}`;
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     loadData(data.docs);
-// }
-
-
-// const loadData = (data) => {
-//     for (const item of data) {
-//         //console.log(item);
-//         const div = document.createElement('div');
-//         div.innerHTML = `<div class="col">
-//         <img src=loadImage('${item.author_key[0]}');
-//         <div class="card-body">
-//                           <h5 class="card-title">Title: ${item.title}</h5>
-//                           <p class="card-text">Author Name: ${item.author_alternative_name[0]}</p>
-//                           <p class="card-text">Author Name: ${item.publisher}</p>
-//                           <p class="card-text">First Year of Publication: ${item.first_publish_year}</p>
-//                       </div>
-//                       </div>
-//   `;
-//         searchResult.appendChild(div);
-//     }
-
-// }
+const searchBook = async () => {
+    searchText = searchField.value;
+    //console.log(searchText);
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    loadData(data.docs);
+}
 
 
-// document.getElementById('error-message').style.display = 'none';
-// document.getElementById('spinner').style.display = 'none';
-// document.getElementById('author-details').textContent = ''
+const loadData = (data) => {
+    for (const item of data) {
+        //console.log(item);
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="col">
+        <img src=loadImage('${item.author_key[0]}');
+        <div class="card-body">
+                          <h5 class="card-title">Title: ${item.title}</h5>
+                          <p class="card-text">Author Name: ${item.author_alternative_name[0]}</p>
+                          <p class="card-text">Author Name: ${item.publisher}</p>
+                          <p class="card-text">First Year of Publication: ${item.first_publish_year}</p>
+                      </div>
+                      </div>
+  `;
+        searchResult.appendChild(div);
+    }
+
+}
+ */
 
 
 const errorMessage = document.getElementById('error-message');
@@ -53,6 +49,7 @@ const spinner = document.getElementById('spinner');
 const authorDetails = document.getElementById('author-details');
 
 const searchResult = document.getElementById('search-result');
+const bookNumber = document.getElementById('book-numbers');
 const searchBook = () => {
     errorMessage.style.display = 'none';
     spinner.style.display = 'none';
@@ -64,16 +61,14 @@ const searchBook = () => {
 
     // This is handling empty string from the user
     if (searchText == '') {
-        // please write something to display
         displayError();
     }
     else {
-        // Display Spinner
-        document.getElementById('spinner').style.display = 'block';
-        document.getElementById('error-message').style.display = 'none';
-        // load data
+        // This is Spinner
+        spinner.style.display = 'block';
+        errorMessage.style.display = 'none';
+        // Load the data
         const url = `https://openlibrary.org/search.json?q=${searchText}`;
-        console.log(url);
         fetch(url)
             .then(res => res.json())
             .then(data => displaySearchResult(data))
@@ -82,10 +77,6 @@ const searchBook = () => {
 }
 
 const displayError = () => {
-    // document.getElementById('error-message').style.display = 'block';
-    // document.getElementById('spinner').style.display = 'none';
-    // document.getElementById('book-numbers').textContent = '';
-    // document.getElementById('author-details').textContent = '';
     errorMessage.style.display = 'block';
     spinner.style.display = 'none';
     authorDetails.textContent = '';
@@ -93,7 +84,7 @@ const displayError = () => {
 }
 // This is display body
 const displaySearchResult = items => {
-    document.getElementById('book-numbers').textContent = '';
+    bookNumber.textContent = '';
 
     searchResult.textContent = '';
 
@@ -102,10 +93,10 @@ const displaySearchResult = items => {
         displayError();
     }
     else {
-        document.getElementById('error-message').style.display = 'none';
-        document.getElementById('spinner').style.display = 'none';
-        document.getElementById('book-numbers').innerText = `Books Found ${bookItem.length}`;
-        // Retrieve each book and display in a card
+        errorMessage.style.display = 'none';
+        spinner.style.display = 'none';
+        bookNumber.innerText = `Found Items ${bookItem.length}`;
+        // Find books and showing in cards
         bookItem.forEach(book => {
             const div = document.createElement('div');
             div.classList.add('col');
@@ -128,24 +119,23 @@ const displaySearchResult = items => {
 
 }
 
-// Fetch author detail
-const loadAuthorDetail = authorId => {
-    fetch(`https://openlibrary.org/authors/${authorId}.json`)
-        .then(res => res.json())
-        .then(res => displayAuthorDetail(res));
+const loadAuthorDetail = async authorId => {
+    const url = `https://openlibrary.org/authors/${authorId}.json`;
+    const response = await fetch(url);
+    const data = await response.json();
+    displayAuthorDetail(data);
 }
 
 const displayAuthorDetail = (authorDetail) => {
-    window.scrollTo(0, 40);
-    const authorShow = document.getElementById('author-details');
-    authorShow.textContent = ''
+    const authorDetails = document.getElementById('author-details');
+    authorDetails.textContent = ''
     const div = document.createElement('div');
-    div.classList.add('card', 'bg-gray', 'text-warning', 'text-center');
+    div.classList.add('card', 'bg-gray', 'text-primary', 'text-center');
     div.innerHTML = `
     <div class="card-body">
         
         <h1 class="card-title">Author name: ${authorDetail.name}</h1>
     </div>
     `;
-    authorShow.appendChild(div);
+    authorDetails.appendChild(div);
 }
